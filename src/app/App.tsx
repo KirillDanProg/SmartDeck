5417808eee48ef65d961e431699d8c5725ee8a13
DIONNIDES <dimakurgan123789@gmail.com>
2022-12-10 21:29:34 +0200

    test:login delete

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,16 @@
-import React from "react";
-import "./App.css";
-import {AppRoutes} from "../layout/AppRoutes/AppRoutes";
-import {Header} from "../layout/Header/Header";
+import React from 'react';
+import './App.css';
+import {AppRoutes} from '../layout/AppRoutes/AppRoutes';
+import {Header} from '../layout/Header/Header';
 
 function App() {
-  return <div className="App">
+    return <div className="App">
 
-    <Header/>
+        <Header/>
 
-    <AppRoutes/>
-    <h2>Login</h2>
-
-    <h1>Dima</h1>
-    <h1>Alex</h1>
-
-  </div>;
+        <AppRoutes/>
+        <h2>Login</h2>
+    </div>;
 }
 
 export default App;
