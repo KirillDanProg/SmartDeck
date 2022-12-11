5449d05b91a4714a377333c3273813753aea388e
KirillDanProg <kirill2898v@gmail.com>
2022-12-11 20:31:06 +0300

    refactor: test

--- src/layout/AppRoutes/AppRoutes.tsx
+++ src/layout/AppRoutes/AppRoutes.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import {PATH} from "./routes";
 import {Page404} from "../404-page/Page404";
-import {Profile} from "../Profile/Profile";
 import {SignUpPage} from "../../features/auth/SignUpPage";
 import {SignInPage} from "../../features/auth/sign-in/SignInPage";
 import {Main} from "../Main/Main";
+import {Profile} from "../../features/profile/Profile";
 
 
 export const AppRoutes = () => {
@@ -14,7 +14,7 @@ export const AppRoutes = () => {
         <>
             <Routes>
                 <Route path={PATH.MAIN}  element={<Main />}>
-                    <Route path={PATH.PROFILE} element={<Profile />} />
+                    <Route path={PATH.PROFILE} element={<Profile/>} />
                     <Route path={PATH.LOGIN} element={<SignInPage />} />
                     <Route path={PATH.SIGN_UP} element={<SignUpPage />} />
                     <Route path="404" element={<Page404 />} />
