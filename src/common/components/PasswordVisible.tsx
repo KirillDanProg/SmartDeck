f9c0a9b6f69f02f560c07dea87a8adce23a56b3a
KirillDanProg <kirill2898v@gmail.com>
2022-12-11 09:50:53 +0300

    feat: password visibility toggle added

---
+++ src/common/components/PasswordVisible.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {VisibilityOn} from "../../assets/icons/VisibilityOn";
+import {VisibilityOff} from "../../assets/icons/VisibilityOff";
+
+export const PasswordVisibleIcon = (props: {isShown: boolean, setShown: (value: boolean) => void}) => {
+
+    const onClickHandler = () => {
+        props.setShown(!props.isShown)
+    }
+
+    return (
+        <span onClick={onClickHandler}>
+            {
+                props.isShown
+                    ? <VisibilityOff/>
+                    : <VisibilityOn/>
+            }
+
+        </span>
+    )
+}
