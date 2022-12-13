7f0fed8dc2d8beae05aaa9e0a415b497cda3811b
KirillDanProg <kirill2898v@gmail.com>
2022-12-13 12:41:02 +0300

    feat: logout flow added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {AppRoutes} from '../layout/AppRoutes/AppRoutes';
 import {Header} from '../layout/Header/Header';
 import {ThemeProvider} from "@mui/material";
 import {theme} from "./theme/theme";
-
+import {useAppSelector} from "./hooks";
+import {useAuthMeMutation} from "../features/auth/authAPI";
+import {BasicModal} from "../common/components/ModalWindow";
+import {selectCurrentError} from "../features/auth/authSlice";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
+
+    const token = useAppSelector(state => state.auth.token)
+
+    const errorMessage = useAppSelector(selectCurrentError)?.message
+
+    const [authMe] = useAuthMeMutation()
+
+    useEffect(() => {
+        if(token) {
+            authMe(token).unwrap()
+        }
+    }, [])
+
     return (
         <ThemeProvider theme={theme}>
 
-            <Header/>
+                <CssBaseline/>
+                {errorMessage && <BasicModal modalTitle="Something went wrong"
+                                             modalText={errorMessage}/>}
 
-            <AppRoutes/>
+                <Header/>
+
+                <AppRoutes/>
 
         </ThemeProvider>
     )
--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,14 +1,26 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice} from "@reduxjs/toolkit";
 import {RootState} from "../../app/store";
 import {authAPI} from "./authAPI";
 
+type StatusType = "idle" | "loading" | "succeeded" | "failed"
 type InitialStateType = {
     token: string | null
     userId: string | null
+    status: StatusType
+    error: SerializedError | null
 }
+type SerializedError = {
+    name?: string
+    message?: string
+    code?: string
+    stack?: string
+}
+
 const initialState: InitialStateType = {
     token: null,
-    userId: null
+    userId: null,
+    status: "idle",
+    error: null
 }
 
 export const authSlice = createSlice({
@@ -17,18 +29,45 @@ export const authSlice = createSlice({
     reducers: {},
     extraReducers: builder => {
         builder
-            .addMatcher(authAPI.endpoints.register.matchFulfilled,
-                (state, {payload}) => {
-                    state.userId = payload._id
+            //login
+            .addMatcher(authAPI.endpoints.login.matchPending,
+                (state) => {
+                    state.status = "loading"
+                    state.error = null
                 }
             )
             .addMatcher(authAPI.endpoints.login.matchFulfilled,
                 (state, {payload}) => {
-                   state.token = payload.token
+                    const {_id, token} = payload
+                    state.status = "succeeded"
+                    state.error = null
+                    state.token = token
+                    state.userId = _id
+                }
+            )
+            .addMatcher(authAPI.endpoints.login.matchRejected,
+                (state, payload) => {
+                    state.status = "failed"
+                    state.error = payload.error
+                }
+            )
+            //logout
+            .addMatcher(authAPI.endpoints.logout.matchRejected,
+                (state, payload) => {
+                    state.status = "failed"
+                    state.error = payload.error
+                }
+            )
+            .addMatcher(authAPI.endpoints.logout.matchFulfilled,
+                (state) => {
+                    state.status = "succeeded"
+                    state.userId = null
+                    state.token = null
                 }
             )
     }
 })
 
 export const selectCurrentUser = (state: RootState) => state.auth.userId;
-export const  selectToken= (state: RootState) => state.auth.token;
+export const selectToken = (state: RootState) => state.auth.token;
+export const selectCurrentError = (state: RootState) => state.auth.error
