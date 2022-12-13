d63a875a7910ce89308f24c5e13245e8276dd09b
Ilya_Karachun <Karachun_ilya94@mail.ru>
2022-12-13 21:12:04 +0300

    feat: design for create password

---
+++ src/features/CreatePassword/CreatePassword.tsx
@@ -0,0 +1,87 @@
+import React, {useState} from 'react';
+import s from "../forgotPassword/ForgotPassword.module.css";
+import Stack from "@mui/material/Stack";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import {useFormik} from "formik";
+import {validationSchema} from "../../common/components/form/yupValidation";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import {InputAdornment} from "@mui/material";
+import {PasswordVisibleIcon} from "../../common/components/PasswordVisible";
+
+export const CreatePassword = () => {
+    const [isShown, setIsShown] = useState(false)
+
+    const passwordType = isShown ? "text" : "password"
+    const formik = useFormik({
+        initialValues: {
+            password: '',
+        },
+        validationSchema,
+        onSubmit: (email) => {
+            formik.resetForm()
+        },
+    })
+    return (
+        <Container
+            maxWidth="sm"
+            sx={{
+                display: 'flex',
+                height: '100vh',
+                justifyContent: 'center',
+            }}
+        >
+            <Paper sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                mt: 7,
+                p: 3,
+                width: 400,
+                height: 350,
+            }}
+            >
+                <Stack sx={{alignItems: 'center'}}
+                       className={s.container}
+                >
+                <h2 className={s.h2}>Create new password</h2>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        error={formik.touched.password && Boolean(formik.errors.password)}
+                        helperText={formik.touched.password && formik.errors.password}
+                        label="Password"
+                        type={passwordType}
+                        id="password"
+                        variant="standard"
+                        {...formik.getFieldProps('password')}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <PasswordVisibleIcon isShown={isShown} setShown={setIsShown}/>
+                                </InputAdornment>
+                            )
+                        }}
+                    />
+                <Typography variant={"inherit"}
+                            className={s.p}>
+                    Create new password and we will send you
+                    <br />
+                    further instructions to email
+                </Typography>
+                <form onSubmit={formik.handleSubmit}>
+                    <Button
+                        variant={"contained"}
+                        color={"primary"}
+                    >
+                        Create new password
+                    </Button>
+                </form>
+                </Stack>
+            </Paper>
+        </Container>
+    );
+};
+
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import { store } from "./app/store";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/App";
 import {ForgotPassword} from "./features/forgotPassword/ForgotPassword";
+import {CreatePassword} from "./features/CreatePassword/CreatePassword";
+import {SignInPage} from "./features/auth/sign-in/SignInPage";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -13,7 +15,8 @@ root.render(
     <BrowserRouter>
       <Provider store={store}>
         {/*<ForgotPassword />*/}
-        <App />
+        {/*<App />*/}
+          <CreatePassword/>
       </Provider>
     </BrowserRouter>
 );
