88774b547941c681ed3b335451ce724a24d974fa
KirillDanProg <kirill2898v@gmail.com>
2022-12-13 17:52:56 +0300

    feat: server error response transformation added

---
+++ src/app/utils/serverErrorTransformed.ts
@@ -0,0 +1,9 @@
+import {SerializedError} from "@reduxjs/toolkit";
+
+export const serverErrorHandler = (error: SerializedError | string) => {
+    if (typeof error === "object") {
+        return error.message
+    }
+
+    return error
+}
