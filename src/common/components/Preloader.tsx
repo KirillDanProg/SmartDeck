866a3d34be6c0ad2a15724a137abfa4593f3d425
KirillDanProg <kirill2898v@gmail.com>
2022-12-13 17:52:14 +0300

    feat: preloader added

---
+++ src/common/components/Preloader.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+export const Preloader = () => {
+    return (
+        <Box sx={{ alignItems: "center", display: "flex", justifyContent: "center", height: "100vh", width: "100vw" }}>
+            <CircularProgress />
+        </Box>
+    );
+}
