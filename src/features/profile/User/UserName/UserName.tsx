0e96445551f192efab5a0490ac0d4991a9af4586
Ilya_Karachun <Karachun_ilya94@mail.ru>
2022-12-13 12:10:45 +0300

    feat: add logic for change name^ but without true logic by rtk query

--- src/features/profile/User/UserName/UserName.tsx
+++ src/features/profile/User/UserName/UserName.tsx
@@ -7,6 +7,7 @@ import BorderColorIcon from '@mui/icons-material/BorderColor'
 import Box from '@mui/material/Box'
 import {Button} from "@mui/material";
 import {useAppSelector} from "../../../../app/hooks";
+import {changeName} from "../../profileSlice";
 
 
 
@@ -22,7 +23,10 @@ export const UserName = () => {
         setUserName(e.currentTarget.value)
     }
 
-    const changeEditeMode = () => setIsEditing(!isEditing)
+    const changeEditeMode = async () => {
+
+        setIsEditing(!isEditing)
+    }
 
     const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter' || e.key === 'Escape') changeEditeMode()
--- src/features/profile/profileSlice.ts
+++ src/features/profile/profileSlice.ts
@@ -1,4 +1,4 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 type InitialStateType = {
     name: string
@@ -11,13 +11,26 @@ const initialState: InitialStateType = {
     email: "incubator@mail.ru"
 }
 
+export const changeNameTC = createAsyncThunk('profile/changeName', async (newName: string, thunkAPI) => {
+    //todo: make right response with rules rtk query
+        return {newName: newName}
+})
+
 export const profileSlice = createSlice({
     name: "profile",
     initialState,
-    reducers: {},
+    reducers: {
+        changeName(state, action: PayloadAction<{ newName: string }>) {
+            state.name = action.payload.newName
+        }
+    },
     extraReducers: builder => {
-        // builder
-
+        builder.addCase(changeNameTC.fulfilled, (state, action) => {
+            state.name = action.payload.newName
+        })
     }
+
 })
+
+export const {changeName} = profileSlice.actions
 
