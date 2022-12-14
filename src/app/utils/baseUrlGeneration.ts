6fe085284d80032a497e72d7dbbb1f33132367c9
KirillDanProg <kirill2898v@gmail.com>
2022-12-14 20:17:16 +0300

    fix: baseUrl generator fixed

--- src/app/utils/baseUrlGeneration.ts
+++ src/app/utils/baseUrlGeneration.ts
@@ -1,5 +1,5 @@
 export const baseUrlGeneration = () => {
     return process.env.NODE_ENV === "development"
-        ? process.env.REACT_APP_BASE_URL
+        ? 'http://localhost:7542/2.0/'
         : process.env.REACT_APP_BASE_URL
 }
