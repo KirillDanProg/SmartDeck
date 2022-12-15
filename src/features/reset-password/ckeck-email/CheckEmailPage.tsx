8c763c56aa79411c27ec9d19571892eeb833d418
KirillDanProg <kirill2898v@gmail.com>
2022-12-15 20:52:37 +0300

    feat: redirect to mail added

--- src/features/reset-password/ckeck-email/CheckEmailPage.tsx
+++ src/features/reset-password/ckeck-email/CheckEmailPage.tsx
@@ -4,20 +4,26 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import checkEmail from '../../../assets/icons/checkEmail.png'
-import Button from '@mui/material/Button';
-import {PATH} from '../../../layout/AppRoutes/routes';
 import {getFromLocalStorage} from '../../../app/utils/local-storage';
-import {Link} from 'react-router-dom';
 import {BasicModal} from '../../../common/components/ModalWindow';
 import {useAppSelector} from "../../../app/hooks";
 import {selectCurrentError} from "../../auth/authSlice";
 import {CustomGridContainer} from "../../../common/components/CustomGridContainer";
+import {getRefFromEmail} from "../../../app/utils/getRefFromEmail";
+import {useNavigate} from "react-router-dom";
 
 
 export const CheckEmailPage = () => {
-    //todo: fix error
+    const navigate = useNavigate()
     const error = useAppSelector(selectCurrentError)
     const [email, setEmail] = useState<string>('');
+
+    const refToMail = getRefFromEmail(email)
+
+    const redirectToLoginHandler = () => {
+        navigate("/login")
+    }
+
     useEffect(() => {
         let result = getFromLocalStorage('email');
         setEmail(String(result))
@@ -29,31 +35,28 @@ export const CheckEmailPage = () => {
                                   modalText="Invalid email or password"
             />}
             <CssBaseline/>
-           <CustomGridContainer>
-                        <Typography component="h1" variant="h3">
-                            Check email
-                        </Typography>
-                        <img style={{maxWidth: '200px'}} src={checkEmail} alt=""/>
-                        <Typography component="h3" variant="h5">
-                            We`ve sent an Email with instructions to
-                        </Typography>
-                        <Typography component="h3" variant="h5">
-                            {
-                                email
-                                    ? <a style={{textDecoration: 'none', color: 'black', fontWeight: 'bold'}}
-                                         href={`mailto:${email}`}>{email}</a>
-                                    : "Something went wrong..."
-                            }
-                        </Typography>
-                        <Button
-                            component={Link}
-                            to={PATH.LOGIN}
-                            type="button"
-                            fullWidth
-                            variant="contained"
-                            sx={{mt: 3, mb: 2}}
-                        >Back to login</Button>
-           </CustomGridContainer>
+            <CustomGridContainer>
+                <Typography component="h1" variant="h3">
+                    Check email
+                </Typography>
+                <img style={{maxWidth: '200px'}} src={checkEmail} alt=""/>
+                <Typography component="h3" variant="h5">
+                    We`ve sent an Email with instructions to
+                </Typography>
+                <Typography component="h3" variant="h5">
+                    {
+                        email
+                            ? <a style={{textDecoration: 'none', color: 'black', fontWeight: 'bold'}}
+                                 href={`mailto:${email}`}>{email}</a>
+                            : "Something went wrong..."
+                    }
+                </Typography>
+                <a
+                    href={refToMail}
+                    onClick={redirectToLoginHandler}
+                    target="_blank"
+                >Check your email</a>
+            </CustomGridContainer>
         </Grid>
     );
 }
