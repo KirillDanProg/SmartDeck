6ae62d6caaeb1c9be5b33c645a680b417ac0ada9
KirillDanProg <kirill2898v@gmail.com>
2023-01-11 14:58:33 +0300

    refactoring

--- src/common/components/CustomGridContainer.tsx
+++ src/common/components/CustomGridContainer.tsx
@@ -25,7 +25,7 @@ export const CustomGridContainer:FC<GridContainerT> = ({children}) => {
           mt: 7,
           p: 3,
           width: 450,
-          height: 500,
+          minHeight: 500,
         }}
       >
         <Stack sx={{alignItems: 'center',justifyContent: 'space-around', }}>
