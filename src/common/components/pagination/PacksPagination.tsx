e6c15d14c381f69cc85366c4236da77aba82eb29
KirillDanProg <kirill2898v@gmail.com>
2024-02-10 02:24:24 +0300

    fix: initial values for table pagination updated

--- src/common/components/pagination/PacksPagination.tsx
+++ src/common/components/pagination/PacksPagination.tsx
@@ -1,7 +1,5 @@
 import React, {FC} from "react";
-import TablePagination from "@mui/material/TablePagination";
-import Pagination from "@mui/material/Pagination";
-import Box from "@mui/material/Box";
+import {TablePagination, Pagination, Box} from "@mui/material";
 import {useQueryParams} from "common/hooks";
 
 //todo fix data for pagination
@@ -21,9 +19,7 @@ export const PacksPagination: FC<PropsType> = ({
   const page = searchParams.get("page") || pageProps;
   const pageCount = searchParams.get("pageCount") || pageCountProps;
   const cardPacksTotalCount = totalCount;
-
   const totalCountPages = Math.round(cardPacksTotalCount / +pageCount);
-
   const handleChangePage = (
     event: React.MouseEvent<HTMLButtonElement> | null,
     newPage: number
@@ -53,18 +49,18 @@ export const PacksPagination: FC<PropsType> = ({
         variant="outlined"
         shape="rounded"
         page={+page}
-        defaultPage={1}
+        defaultPage={0}
         onChange={currentPageHandler}
       />
       <TablePagination
         sx={{mt: -1}}
         component="div"
         count={cardPacksTotalCount}
-        page={+page}
+        page={+page - 1}
         onPageChange={handleChangePage}
         rowsPerPage={+pageCount}
         onRowsPerPageChange={handleChangeRowsPerPage}
-        rowsPerPageOptions={[1, 5, 10, 25]}
+        rowsPerPageOptions={[1, 4, 10, 25]}
       />
     </Box>
   );
