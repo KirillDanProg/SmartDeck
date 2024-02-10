f05d24fe776fc95a2b882c14d252174a19116b39
KirillDanProg <kirill2898v@gmail.com>
2024-02-10 19:48:34 +0300

    feat: location state used to redirect back to "fromPage"

--- src/features/auth/sign-in/SignInPage.tsx
+++ src/features/auth/sign-in/SignInPage.tsx
@@ -1,27 +1,30 @@
 import {Form} from "../../../common/components/form/Form";
 import {ILoginRequest} from "../authModels";
 import {useLoginMutation} from "../authApi";
-import {CustomGridContainer} from "common/components";
-import Avatar from "@mui/material/Avatar";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import {FlexContainer} from "common/components";
+import {Typography, Box, Avatar} from "@mui/material";
 import {PATH} from "layout/AppRoutes/routes";
-import {useNavigate} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
+import {useEffect} from "react";
 
 export const SignInPage = () => {
-  const [login, {error, isSuccess}] = useLoginMutation();
+  const [login, {isSuccess, data}] = useLoginMutation();
   const navigate = useNavigate();
+  const location = useLocation();
+  const fromPage = location.state.from.path || PATH.MAIN;
 
-  if (isSuccess) {
-    navigate(PATH.MAIN, {replace: true});
-  }
+  useEffect(() => {
+    if (isSuccess) {
+      navigate(fromPage);
+    }
+  }, [isSuccess]);
 
   async function signInHandler(formData: ILoginRequest) {
     await login(formData);
   }
 
   return (
-    <CustomGridContainer>
+    <FlexContainer>
       <Box sx={style}>
         <Avatar sx={{m: 1, bgcolor: "secondary.main"}} />
         <Typography component="h1" variant="h5">
@@ -29,7 +32,7 @@ export const SignInPage = () => {
         </Typography>
         <Form formType="sign-in" callback={signInHandler} />
       </Box>
-    </CustomGridContainer>
+    </FlexContainer>
   );
 };
 
